00b0fb1a8ef3762a608dfeb78eab915b8a1196ab
Amol Nayak <amolnayak311@gmail.com>
2018-05-01 23:04:54 -0700

    LinearRegressionDataFrame

---
+++ spark-scala/src/main/scala/org/sparkscala/LinearRegressionDataFrame.scala
@@ -0,0 +1,61 @@
+package org.sparkscala
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+import org.apache.spark.ml.regression.LinearRegression
+import org.apache.spark.mllib.evaluation.RegressionMetrics
+
+
+
+object LinearRegressionDataFrame {
+ 
+  def main(args: Array[String]) {
+    
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    val spark = SparkSession
+                  .builder
+                  .appName("LinearRegressionDataFrame")
+                  .master("local[*]")
+                  .getOrCreate()
+                  
+    import spark.implicits._
+    
+    val inputLines = spark.sparkContext.textFile("data/regression.txt")
+    
+    val colNames = Seq("label", "features")
+    val data = inputLines
+                  .map(_.split(","))
+                  .map(x => (x(0).toDouble, Vectors.dense(x(1).toDouble)))
+    
+                  
+     val dataFrame = data.toDF(colNames: _*)
+     
+     val Array(train, test) = dataFrame.randomSplit(Array(0.8, 0.2))
+     
+           
+     val regression = new LinearRegression()
+                      .setMaxIter(10)
+                      .setElasticNetParam(0.8)
+                      .setRegParam(0.01)
+                      
+     val lrModel = regression.fit(train)
+     
+     val modelSummary = lrModel.summary
+     
+     println(s"RMSE: ${modelSummary.rootMeanSquaredError}, R2: ${modelSummary.r2}\n")
+     
+     val results = lrModel.transform(test)
+     results.show()
+     
+     val rdd = results.rdd.map(r => (r.getAs[Double]("label"), r.getAs[Double]("prediction")))
+     
+     val regMetrics = new RegressionMetrics(rdd)    
+     println(s"RMSE on test data is ${regMetrics.rootMeanSquaredError}, r2 is ${regMetrics.r2}\n")
+                      
+                      
+                      
+                      
+  }
+}
